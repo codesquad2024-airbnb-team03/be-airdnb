72c0bcb25d6e8338f57063fd2154cf627276f198
최수연 <tndus5383@gmail.com>
2024-06-03 16:19:37 +0900

    chore: 프로젝트 init 세팅

---
+++ src/main/java/team03/airdnb/AirdnbApplication.java
@@ -0,0 +1,13 @@
+package team03.airdnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdnbApplication.class, args);
+	}
+
+}
