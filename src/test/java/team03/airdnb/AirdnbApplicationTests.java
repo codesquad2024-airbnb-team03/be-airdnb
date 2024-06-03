c16338ad774cc6720a8d003ae80f772daa4e7a8a
최수연 <tndus5383@gmail.com>
2024-06-03 16:19:37 +0900

    chore: 프로젝트 init 세팅

---
+++ src/test/java/team03/airdnb/AirdnbApplicationTests.java
@@ -0,0 +1,13 @@
+package team03.airdnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirdnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
