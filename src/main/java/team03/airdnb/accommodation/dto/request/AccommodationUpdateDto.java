aee9bb5138b97b62d075a1b71facd6e9d8a42fb3
Daniel <923daniel@naver.com>
2024-06-07 14:39:07 +0900

    feat(#8): Accommodation Update를 위한 dto 구현

---
+++ src/main/java/team03/airdnb/accommodation/dto/request/AccommodationUpdateDto.java
@@ -0,0 +1,38 @@
+package team03.airdnb.accommodation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import team03.airdnb.accommodation.Accommodation;
+import team03.airdnb.accommodation.Address;
+import team03.airdnb.user.User;
+
+@Getter
+@AllArgsConstructor
+public class AccommodationUpdateDto {
+
+    private Long id;
+    private String name;
+    private String profileImg;
+    private Address address;
+    private Long price;
+    private int headcount;
+    private int bedCount;
+    private int bedroomCount;
+    private int bathroomCount;
+    private Long hostId;
+
+    public Accommodation toEntity(User host){
+        return Accommodation.builder()
+                .id(this.id)
+                .name(this.name)
+                .profileImg(this.profileImg)
+                .address(this.address)
+                .price(this.price)
+                .headCount(this.headcount)
+                .bedCount(this.bedCount)
+                .bedroomCount(this.bedroomCount)
+                .bathroomCount(this.bathroomCount)
+                .host(host)
+                .build();
+    }
+}
