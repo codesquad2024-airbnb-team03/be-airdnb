f646445a524a52195ada4eb14a82301fbe9c5f51
Daniel <923daniel@naver.com>
2024-06-10 17:32:38 +0900

    feat(#15): User의 예약 정보 반환 기능 구현

--- src/main/java/team03/airdnb/user/UserController.java
+++ src/main/java/team03/airdnb/user/UserController.java
@@ -1,6 +1,5 @@
 package team03.airdnb.user;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team03.airdnb.accommodation.dto.response.AccommodationListDto;
+import team03.airdnb.reservation.dto.response.ReservationShowDto;
 import team03.airdnb.review.dto.response.ReviewShowDto;
+
+import java.util.List;
 
 @RequestMapping("/users")
 @RestController
@@ -16,6 +18,11 @@ import team03.airdnb.review.dto.response.ReviewShowDto;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/{userId}/reservations")
+    public ResponseEntity<List<ReservationShowDto>> showReservation(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.showReservations(userId));
+    }
 
     @GetMapping("/{userId}/favorites")
     public ResponseEntity<List<AccommodationListDto>> showFavoriteList(@PathVariable Long userId) {
--- src/main/java/team03/airdnb/user/UserService.java
+++ src/main/java/team03/airdnb/user/UserService.java
@@ -1,20 +1,36 @@
 package team03.airdnb.user;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team03.airdnb.accommodation.Accommodation;
 import team03.airdnb.accommodation.dto.response.AccommodationListDto;
 import team03.airdnb.favorite.Favorite;
+import team03.airdnb.reservation.Reservation;
+import team03.airdnb.reservation.dto.response.ReservationShowDto;
 import team03.airdnb.review.Review;
 import team03.airdnb.review.dto.response.ReviewShowDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public List<ReservationShowDto> showReservations(Long userId) {
+        User user = userRepository.findById(userId).get();
+        List<Reservation> reservations = user.getReservations();
+
+        List<ReservationShowDto> reservationShowDtos = new ArrayList<>();
+
+        for (Reservation reservation : reservations) {
+            reservationShowDtos.add(ReservationShowDto.of(reservation));
+        }
+
+        return reservationShowDtos;
+    }
 
     public List<AccommodationListDto> showFavoriteList(Long userId) {
         User user = userRepository.findById(userId).get();
