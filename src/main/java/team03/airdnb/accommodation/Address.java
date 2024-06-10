bcbe0a697072b6a899231c5da1326f0f14bc2634
최수연 <tndus5383@gmail.com>
2024-06-10 22:16:27 +0900

    fix: 값타입 변경 불가능하게 설계

--- src/main/java/team03/airdnb/accommodation/Address.java
+++ src/main/java/team03/airdnb/accommodation/Address.java
@@ -2,13 +2,21 @@ package team03.airdnb.accommodation;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String firstAddress; // 광역시/도
     private String secondAddress; // 시/군/구
     private String thirdAddress; // 도로명
+
+    protected Address() {
+    }
+
+    public Address(String firstAddress, String secondAddress, String thirdAddress) {
+        this.firstAddress = firstAddress;
+        this.secondAddress = secondAddress;
+        this.thirdAddress = thirdAddress;
+    }
 }
