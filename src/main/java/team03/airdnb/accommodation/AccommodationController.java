f8d93a8516f648db20391e29be8d282d2541068b
최수연 <tndus5383@gmail.com>
2024-06-12 17:51:37 +0900

    feat: 필터 로직 api를 위한 서비스, 컨트롤러 메서드 구현

--- src/main/java/team03/airdnb/accommodation/AccommodationController.java
+++ src/main/java/team03/airdnb/accommodation/AccommodationController.java
@@ -1,16 +1,24 @@
 package team03.airdnb.accommodation;
 
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import team03.airdnb.accommodation.dto.request.AccommodationSaveDto;
 import team03.airdnb.accommodation.dto.request.AccommodationUpdateDto;
 import team03.airdnb.accommodation.dto.response.AccommodationListDto;
 import team03.airdnb.accommodation.dto.response.AccommodationShowDto;
-
-import java.net.URI;
-import java.util.List;
 
 @RequestMapping("/accommodations")
 @RestController
@@ -20,7 +28,8 @@ public class AccommodationController {
     private final AccommodationService accommodationService;
 
     @PostMapping
-    public ResponseEntity<Void> createAccommodation(@RequestBody AccommodationSaveDto accommodationSaveDto, UriComponentsBuilder uriComponentsBuilder) {
+    public ResponseEntity<Void> createAccommodation(@RequestBody AccommodationSaveDto accommodationSaveDto,
+                                                    UriComponentsBuilder uriComponentsBuilder) {
         Accommodation createdAccommodation = accommodationService.createAccommodation(accommodationSaveDto);
         URI location = uriComponentsBuilder.path("/accommodations/{id}")
                 .buildAndExpand(createdAccommodation.getId())
@@ -59,5 +68,17 @@ public class AccommodationController {
         return ResponseEntity
                 .noContent()
                 .build();
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<Accommodation>> filterAccommodations(@RequestParam LocalDate checkIn,
+                                                                    @RequestParam LocalDate checkOut,
+                                                                    @RequestParam Double minPrice,
+                                                                    @RequestParam Double maxPrice,
+                                                                    @RequestParam Integer capacity) {
+        List<Accommodation> accommodationsByFilters = accommodationService.findAccommodationsByFilters(checkIn,
+                checkOut, minPrice, maxPrice, capacity);
+
+        return ResponseEntity.ok(accommodationsByFilters);
     }
 }
