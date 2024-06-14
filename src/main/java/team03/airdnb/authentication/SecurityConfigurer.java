430dc249ba80a6d4da54b09dac59444b343bf4a8
Daniel <923daniel@naver.com>
2024-06-14 19:02:46 +0900

    feat(#19): SecurityConfigurer에 oauth2Login 설정 구현

--- src/main/java/team03/airdnb/authentication/SecurityConfigurer.java
+++ src/main/java/team03/airdnb/authentication/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package team03.airdnb.authentication;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import team03.airdnb.authentication.jwt.JwtRequestFilter;
+import team03.airdnb.authentication.oauth.CustomOAuth2UserService;
+import team03.airdnb.authentication.oauth.OAuth2LoginSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -20,19 +26,36 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfigurer {
 
     private final JwtRequestFilter jwtRequestFilter;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/authenticate", "/users").permitAll()
+                        .requestMatchers("/authenticate", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated()
                 )
+
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .authorizationEndpoint(authorization -> authorization
+                                .baseUri("/login/oauth")
+                        )
+                        .userInfoEndpoint(c -> c.userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler)
+                )
+
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
 
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_FORBIDDEN))
+                );
 
         return http.build();
     }
