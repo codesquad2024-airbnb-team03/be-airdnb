21df713a2be10d2f31a772ab4226f73ac32143f5
Daniel <923daniel@naver.com>
2024-06-17 16:11:10 +0900

    feat(#26): Filter에 위도 경도 조건 추가

--- src/main/java/team03/airdnb/accommodation/AccommodationRepositoryImpl.java
+++ src/main/java/team03/airdnb/accommodation/AccommodationRepositoryImpl.java
@@ -1,17 +1,16 @@
 package team03.airdnb.accommodation;
 
-import static team03.airdnb.accommodation.QAccommodation.accommodation;
-import static team03.airdnb.reservation.QReservation.reservation;
-
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import team03.airdnb.accommodation.dto.request.AccommodationFilterDto;
+import team03.airdnb.reservation.QReservation;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public class AccommodationRepositoryImpl implements AccommodationRepositoryCustom {
@@ -32,35 +31,46 @@ public class AccommodationRepositoryImpl implements AccommodationRepositoryCusto
         Double minPrice = filterDto.getMinPrice();
         Double maxPrice = filterDto.getMaxPrice();
         Integer capacity = filterDto.getCapacity();
+        Double latitude = filterDto.getCurrentLatitude();
+        Double longitude = filterDto.getCurrentLongitude();
 
         if (checkIn != null && checkOut != null) {
             // 서브쿼리 조건 작성
-            BooleanExpression subQueryCondition = reservation.accommodation.id.eq(accommodation.id)
-                    .and(reservation.checkIn.loe(checkOut))
-                    .and(reservation.checkOut.goe(checkIn));
+            BooleanExpression subQueryCondition = QReservation.reservation.accommodation.id.eq(QAccommodation.accommodation.id)
+                    .and(QReservation.reservation.checkIn.loe(checkOut))
+                    .and(QReservation.reservation.checkOut.goe(checkIn));
 
             // NOT EXISTS 조건 추가
             builder.and(JPAExpressions.selectOne()
-                    .from(reservation)
+                    .from(QReservation.reservation)
                     .where(subQueryCondition)
                     .notExists());
         }
 
         // 기타 조건 추가
         if (minPrice != null) {
-            builder.and(accommodation.price.goe(minPrice));
+            builder.and(QAccommodation.accommodation.price.goe(minPrice));
         }
 
         if (maxPrice != null) {
-            builder.and(accommodation.price.loe(maxPrice));
+            builder.and(QAccommodation.accommodation.price.loe(maxPrice));
         }
 
         if (capacity != null) {
-            builder.and(accommodation.maxHeadCount.goe(capacity));
+            builder.and(QAccommodation.accommodation.maxHeadCount.goe(capacity));
+        }
+
+        // 위도와 경도 범위 조건 추가
+        if (latitude != null && longitude != null) {
+            BooleanExpression withinRadiusCondition = QAccommodation.accommodation.latitude.goe(latitude - 0.01)
+                    .and(QAccommodation.accommodation.latitude.loe(latitude + 0.01))
+                    .and(QAccommodation.accommodation.longitude.goe(longitude - 0.01))
+                    .and(QAccommodation.accommodation.longitude.loe(longitude + 0.01));
+            builder.and(withinRadiusCondition);
         }
 
         // 주어진 조건에 따라 숙박 목록 조회
-        return queryFactory.selectFrom(accommodation)
+        return queryFactory.selectFrom(QAccommodation.accommodation)
                 .where(builder)
                 .fetch();
     }
