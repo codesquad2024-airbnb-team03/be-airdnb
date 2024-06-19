ae7ea3345fb425d530119c41c487be5901f3b1c6
Daniel <923daniel@naver.com>
2024-06-19 17:11:05 +0900

    refactor: 스위치 분기문 대신 enum 사용

--- src/main/java/team03/airdnb/accommodation/Address.java
+++ src/main/java/team03/airdnb/accommodation/Address.java
@@ -3,16 +3,15 @@ package team03.airdnb.accommodation;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String firstAddress; // 광역시/도
     private String secondAddress; // 시/군/구
     private String thirdAddress; // 도로명
-
-    protected Address() {
-    }
 }
