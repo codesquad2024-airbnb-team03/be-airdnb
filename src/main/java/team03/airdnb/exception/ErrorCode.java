0dacd34c20c5f3dbf438b3a9d362bed92be1f61c
Daniel <923daniel@naver.com>
2024-06-20 17:21:59 +0900

    feat(#32): Error 상태와 메시지를 저장하는 enum 구현

---
+++ src/main/java/team03/airdnb/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package team03.airdnb.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    DUPLICATE_NAME(HttpStatus.CONFLICT, "Name already exists"),
+    HOST_NOT_FOUND(HttpStatus.NOT_FOUND, "Host not found"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    ACCOMMODATION_NOT_FOUND(HttpStatus.NOT_FOUND, "Accommodation not found"),
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "Review not found"),
+    AMENITY_NOT_FOUND(HttpStatus.NOT_FOUND, "Amenity not found");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
